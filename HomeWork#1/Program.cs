4639011d840cc3a63a17a18ea4926c4612082afd
PabloKalinovsky <kalinapavel87@mail.ru>
2022-10-18 00:33:29 +0900

    Inisiat commit

---
+++ HomeWork#1/Program.cs
@@ -0,0 +1,77 @@
+﻿// ДЗ 1.
+// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+Console.Write("Input first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+    Console.WriteLine($"Max number is {a} ");
+else
+    Console.WriteLine($"Max number is {b} ");
+*/
+
+
+//Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(max < b)
+{
+    max = b;
+}
+    if(max < c)
+    {
+        max = c;
+    }
+    
+Console.WriteLine($"Max number is {max} ");
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.Write("Input number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+*/
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Input number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+
+while(current <= n)
+{
+    if(current % 2 == 0)
+    {
+     Console.WriteLine(current + " "); 
+     current++;  
+    }
+    else
+    current++;
+}
+*/
+
+
